0d79d358a2ae4ecab75e86a1e92929c0157785e6
ALP300 <aitorpalomino1@gmail.com>
2024-05-11 14:03:41 -0500

    Se realizó el ejercicio de bucles

---
+++ bucles.py
@@ -0,0 +1,14 @@
+#Escribir un programa que pida al usuario un 
+#número entero y muestre por pantalla un triángulo
+#rectángulo como el de más abajo.
+#1
+#3 1
+#5 3 1
+#7 5 3 1
+#9 7 5 3 1
+#20
+n= int(input("Introduce la altura del triángulo (entero positivo): ")) 
+for i in range(1,n+1,2):   #1  3 5  7
+    for j in range(i,0,-2):
+        print(j, end=" ")
+    print("") 
